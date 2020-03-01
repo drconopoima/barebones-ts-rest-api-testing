8d18b1bdeff09152610e05aed5a643213d74ebe9
Luis Díaz Manzo <luis@drconopoima.com>
2020-03-01 18:34:04 +0100

    Jest test to verify main '/' endpoint responds

--- __tests__/integrity.test.js
+++ __tests__/integrity.test.js
@@ -1,7 +1,18 @@
+const supertest = require("supertest");
+const app = require("../src/server");
+
 describe("Verify Jest", () => {
-  it("is working correctly", () => {
-    expect(1).toBe(1);
+  it("is working correctly", async (done) => {
+    expect(1).toBe(1)
+    done()
   });
 });
 
-
+describe("Main app Endpoint", () => {
+  it("responds to request", async () => {
+    // Act
+    const response = await supertest(app).get("/");
+    // Assert
+    expect(response.status).toBe(200);
+  });
+});
---
+++ src/server/index.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express();
+const port = 8085;
+
+module.exports = app.listen(port);
