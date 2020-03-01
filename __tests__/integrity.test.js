d3e69e562a12245610f6591533ad0546dc9a52f2
Luis Díaz Manzo <luis@drconopoima.com>
2020-03-01 17:03:42 +0100

    Add Supertest dependency. Package.json fields configuration. Formatting.

--- __tests__/integrity.test.js
+++ __tests__/integrity.test.js
@@ -1,5 +1,7 @@
-describe('Verify Jest library', () => {
-    it('is working correctly', () => {
-        expect(1).toBe(1)
-    })
-})
+describe("Verify Jest", () => {
+  it("is working correctly", () => {
+    expect(1).toBe(1);
+  });
+});
+
+
